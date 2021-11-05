7ede2d2947695497a8d807279fa171a6eb29fc4a
Nate Duff <nduff@protonmail.com>
2021-11-05 15:45:40 +0100

    uncommented link

--- src/Components/ResultCard.js
+++ src/Components/ResultCard.js
@@ -3,35 +3,36 @@ import { Link } from "react-router-dom";
 
 function ResultCard({ flight, convertTime }) {
   return (
-    // <Link to={`/flights/${flight.cityFrom}/${flight.cityTo}/${flight.dTime}`}>
-    <div className="result-card">
-      <div className="numbers">
-        <h1>&euro;{flight.price}</h1>
-        <div>
-          <h6>Departs: {convertTime(flight.dTime)}</h6>
-          <h6>Arrives: {convertTime(flight.aTime)}</h6>
-        </div>
-      </div>
-      <div className="routes">
-        {flight.route.map((r, i) => (
-          <div key={i}>
-            <img
-              src={`https://images.kiwi.com/airlines/64x64/${r.airline}.png`}
-            />
+    <Link to={`/flights/${flight.cityFrom}/${flight.cityTo}/${flight.dTime}`}>
+      <div className="result-card">
+        <div className="numbers">
+          <h1>&euro;{flight.price}</h1>
+          <div>
             <h6>
-              <b>
+              <b>Departs:</b> {convertTime(flight.dTime)}
+            </h6>
+            <h6>
+              <b>Arrives:</b> {convertTime(flight.aTime)}
+            </h6>
+          </div>
+        </div>
+        <div className="routes">
+          {flight.route.map((r, i) => (
+            <div key={i}>
+              <img
+                src={`https://images.kiwi.com/airlines/64x64/${r.airline}.png`}
+              />
+              <h6>
                 {r.airline}
                 {r.flight_no}
-              </b>
-            </h6>
-            <h6>{convertTime(r.dTime)}-</h6>
-            <h6>{convertTime(r.aTime)}</h6>
-          </div>
-        ))}
+              </h6>
+              <h6>{convertTime(r.dTime)}-</h6>
+              <h6>{convertTime(r.aTime)}</h6>
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
-
-    // </Link>
+    </Link>
   );
 }
 
