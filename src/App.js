9f0cc5788d0cd4d130adf10ec0a551c86af511ca
Nate Duff <nduff@protonmail.com>
2021-11-05 10:33:46 +0100

    flight and time

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { DateTime } from "luxon";
+
+import { useEffect, useState } from "react";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  const url =
+    "https://api.skypicker.com/flights?fly_from=PRG&fly_to=VLC&date_from=06/11/2021&date_to=06/11/2021&direct_flights=0&locale=en&partner=data4youcbp202106&curr=EUR&price_from=1&price_to=10000";
+
+  async function fetchData() {
+    const resp = await fetch(url);
+    const result = await resp.json();
+    // console.log(result);
+    setData(result.data);
+  }
+
+  function convertTime(unixTime) {
+    const time = DateTime.fromMillis(unixTime * 1000).toFormat("hh:mm");
+    return time;
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      bollocks
+      {console.log(data)}
+      {data.map((flight, index) => (
+        <div>
+          <div>
+            <div>{convertTime(flight.dTime)}</div>
+            {flight.route.map((r, i) => (
+              <div>
+                {r.airline}
+                {r.flight_no}
+              </div>
+            ))}
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
